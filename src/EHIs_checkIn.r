6777ca43d5f397fd0d35b3f1828541388061d82f
ivanhanigan <ivan.hanigan@gmail.com>
2013-01-30 21:36:32 +1100

    delete old reporting files, make way for package

--- src/EHIs_checkIn.r
+++
@@ -1,8 +0,0 @@
-###############################################################################
-# newnode qc graph
-	
- # do graph
- png(paste('reports/',stnlabl, '.png', sep = ''))
- with(analyte, plot(dates, avtemp, type = 'l', main =  stnlabl))
- dev.off()
- 
