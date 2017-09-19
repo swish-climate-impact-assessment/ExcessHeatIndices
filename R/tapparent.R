561ac88c7fb77e75f2fb3abeb89bb046b560462a
ivanhanigan <ivan.hanigan@gmail.com>
2017-09-19 14:48:05 +1000

    updates to tapparent

--- R/tapparent.R
+++ R/tapparent.R
@@ -1,8 +1,8 @@
 
 # steadman 1994 (and BoM Ta + 0.33×e − 0.70×ws − 4.00)
-tapparent <- function(dat, ta = "maxave", vprph = "vprph15", ws = 0){
+tapparent <- function(ta, vprph, ws = 0){
 
-  dat$tappmax <- dat[,ta] + (0.33 * dat[,vprph]) - (0.70 * ws) - 4.00
+  tapparent <- ta + (0.33 * vprph) - (0.70 * ws) - 4.00
 
-  return(dat)
+  return(tapparent)
 }
